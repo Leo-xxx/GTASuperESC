c5b511b66140a4142e0621b3d30c042cdcd148c6
wgzeyu <785777793@qq.com>
2018-09-27 03:27:36 +0800

    更新WS服务器列表

--- php/serverlist.php
+++ php/serverlist.php
@@ -1,4 +1,9 @@
 <?php 
 header("content-type: text/XML");
-echo(file_get_contents("serverlist.txt"));
+$b64 = $_GET["b64"];
+if($b64=="1"){
+	echo(file_get_contents("serverlistb64.txt"));
+} else {
+	echo(file_get_contents("serverlist.txt"));
+}
 ?>
